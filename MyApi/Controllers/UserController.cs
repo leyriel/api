1ca741cce6c861866c621db0ede46aaf46c201f0
Leyriel M <Leyriel@LIVE.FR>
2017-10-10 16:50:59 +0200

    add a user password hash security

--- MyApi/Controllers/UserController.cs
+++ MyApi/Controllers/UserController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyApi.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace MyApi.Controllers
 {
@@ -22,14 +24,20 @@ namespace MyApi.Controllers
             _context = context;
         }
 
-        // GET: api/User test
+        /*
+         *GET: api/users test
+         *Get all users
+         */
         [HttpGet]
         public IEnumerable<Users> GetUsers()
         {
             return _context.Users;
         }
 
-        // GET: api/User/5
+        /*
+         *GET: api/users/1 
+         *Get user by id
+         */
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUsers([FromRoute] int id)
         {
@@ -48,7 +56,10 @@ namespace MyApi.Controllers
             return Ok(users);
         }
 
-        // PUT: api/User/5
+        /*
+         *GET: api/users/1 
+         *Edit user by id
+         */
         [HttpPut("{id}")]
         public async Task<IActionResult> PutUsers([FromRoute] int id, [FromBody] Users users)
         {
@@ -83,22 +94,36 @@ namespace MyApi.Controllers
             return NoContent();
         }
 
-        // POST: api/User
+        /*
+         *GET: api/users/create
+         *Create new user
+         */
         [HttpPost("create")]
-        public async Task<IActionResult> PostUsers([FromBody] Users users)
+        public async Task<IActionResult> PostUsers([FromBody] Users user)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            _context.Users.Add(users);
+            //encrypte password                        
+            System.Diagnostics.Debug.WriteLine("KEY-GENERATOR", this.KeyGenerator());            
+            var hashedPassword = this.SHA1(user.Password + this.KeyGenerator());
+            System.Diagnostics.Debug.WriteLine("PASSWORD-HASHED", this.SHA1(user.Password));
+
+            user.Skey = this.KeyGenerator();
+            user.Password = hashedPassword;            
+
+            _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetUsers", new { id = users.id }, users);
+            return CreatedAtAction("GetUsers", new { id = user.id }, user);
         }
 
-        // DELETE: api/User/5
+        /*
+         *GET: api/users/1 
+         *Delete user by id
+         */
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUsers([FromRoute] int id)
         {
@@ -122,6 +147,56 @@ namespace MyApi.Controllers
         private bool UsersExists(int id)
         {
             return _context.Users.Any(e => e.id == id);
+        }
+
+        private string EncryptPassword(string password)
+        {           
+            return password;
+        }
+
+        private string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string        
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+        private string KeyGenerator()
+        {
+            Guid g = Guid.NewGuid();
+            string GuidString = Convert.ToBase64String(g.ToByteArray());
+            GuidString = GuidString.Replace("=", "");
+            GuidString = GuidString.Replace("+", "");
+
+            return this.CalculateMD5Hash(GuidString);
+        }
+
+        private string SHA1(string input)
+        {
+            byte[] hash;
+
+            using (var sha1 = new SHA1CryptoServiceProvider())
+            {
+                hash = sha1.ComputeHash(Encoding.Unicode.GetBytes(input));
+            }
+            var sb = new StringBuilder();
+
+            foreach (byte b in hash) sb.AppendFormat("{0:x2}", b);
+
+            return sb.ToString();
         }
     }
 }
